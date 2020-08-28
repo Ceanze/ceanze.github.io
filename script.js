faa2331c50e0f45a7fd4bb1a7bb4654f1bcb2b7b
Ceanze <simnylen@gmail.com>
2020-08-28 21:51:20 +0200

    Updated design

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
-/* FUNCTIONS */
-function aboutClick() {
-    let aboutBtn = document.getElementById("about");
-    let aboutPage = document.getElementById("about-page");
-    aboutBtn.addEventListener("click", () => {
-        aboutPage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-    });
+function animateOut() {
+    let left = document.getElementsByClassName("left-bar")[0];
+    let right = document.getElementsByClassName("right-bar")[0];
+    
+    if (!left.classList.contains("active")) {
+        left.classList.add("active")
+        right.classList.add("active")
+    }
 }
 
-function projectsClick() {
-    let projBtn = document.getElementById("projects");
-    projBtn.addEventListener("click", () => {
-        window.location.replace("https://github.com/Ceanze");
-    });
+function animateIn() {
+    let left = document.getElementsByClassName("left-bar")[0];
+    let right = document.getElementsByClassName("right-bar")[0];
+    
+    left.classList.remove("active");
+    right.classList.remove("active");
 }
 
-function cvClick() {
+function toggleBars() {
+    let left = document.getElementsByClassName("left-bar")[0];
+    if (left.classList.contains("active")) {
+        animateIn();
+    } else {
+        animateOut();
+    };
+}
 
+function setActive(newActive) {
+    let items = document.getElementById("nav-items").getElementsByTagName("li");
+    for (let item = 0; item < items.length; item++) {
+        if (items[item].classList.contains("active")) {
+            items[item].classList.remove("active");
+        };
+    };
+    newActive.classList.add("active");
+}
+
+function homeClick(id) {
+    animateIn()
+    setActive(id)
+}
+
+function aboutClick(id) {
+    animateOut()
+    setActive(id)
+}
+
+function  projectsClick(id) {
+    animateOut()
+    setActive(id)
+}
+
+function cvClick(id) {
+    animateOut()
+    setActive(id)
+}
+
+function contactClick(id) {
+    animateOut()
+    setActive(id)
 }
 
 /* MAIN */
 function main() {
-    aboutClick();
-    projectsClick();
+    
 }
 main();
