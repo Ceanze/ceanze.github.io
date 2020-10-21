406b0c529957c8fb68bc0c58485678dcb7ff32ff
Ceanze <simnylen@gmail.com>
2020-10-21 21:55:50 +0200

    Cleanup

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-var activeAnimation = "default";
-
-function setContent(oldContent, newContent) {
-    // Remove visibility for old content
-    if (oldContent != null && oldContent != "default") {
-        let prevContent = document.getElementsByClassName(oldContent + "Content")[0];
-        if (prevContent.classList.contains("visible")) {
-            prevContent.classList.remove("visible");
-        }
-    }
-
-    // Add visibility to new content
-    if (newContent != null && newContent != "default") {
-        let content = document.getElementsByClassName(newContent + "Content")[0];
-        content.classList.add("visible");
-    }
-}
-
-function setAnimation(newAnimation) {
-    let left = document.getElementsByClassName("left-bar")[0];
-    let right = document.getElementsByClassName("right-bar")[0];
-
-    setContent(activeAnimation, newAnimation);
-    
-    // Remove previous active if it exist
-    if (activeAnimation != null) {
-        if (left.classList.contains(activeAnimation)) {
-            left.classList.remove(activeAnimation)
-        }
-        if (right.classList.contains(activeAnimation)) {
-            right.classList.remove(activeAnimation)
-        }
-    }
-
-    // Add new animation if a new animation is requested
-    if (newAnimation != null) {
-        left.classList.add(newAnimation);
-        right.classList.add(newAnimation);
-    }
-
-
-    // Set active animation to new animation
-    activeAnimation = newAnimation;
-}
-
 function setActive(newActive, index) {
     let items = document.getElementById("nav-items").getElementsByTagName("li");
     for (let item = 0; item < items.length; item++) {
@@ -59,10 +14,6 @@ function setActive(newActive, index) {
 
 /* MAIN */
 function main() {
-    window.onload = function() {
-        this.setAnimation(null)
-    }
-
     setActive(document.getElementById("nav-items").children[0], 0);
 }
 main();
